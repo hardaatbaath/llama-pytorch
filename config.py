bd07eed2387cbb1dfd9c7c58ca0ffb3f8a196de0
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2024-01-01 20:52:58 +0530

    Added model.py, README, config.py and requirement.txt

---
+++ config.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class ModelArgs:
+    dim: int = 4096
+    n_layers: int = 32
+    n_heads: int = 32 # Number of heads for the queries
+    n_kv_heads: Optional[int] = None # Number of heads for K and V
+    vocab_size: int = -1 # Later set when we load the tokenizer
+
+    # To keep the hidden dimention of the FF layer
+    multiple_of: int = 256
+    ffn_dim_multiplier: Optional[float] = None
+    norm_eps: float = 1e-5
+
+    # Needed for KV cache
+    max_batch_size: int = 32
+    max_seq_len: int = 2048
+
+    device: str = None
